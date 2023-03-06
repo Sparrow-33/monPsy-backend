72aeb72c3603225a01f53774830aa2c8c1be3d54
Haytham <elhaoudihaytham2001@gmail.com>
2023-03-06 11:18:13 +0100

    security -> roles config

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.config;
 import com.example.demo.dao.UserDao;
 import com.example.demo.entities.AppUser;
 import com.example.demo.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,36 +25,54 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 
 public class SecurityConfig  {
     private final JwtAuthFilter jwtAuthFilter;
     private final UserDao userDao;
     private final UserService userService;
 
-    @Autowired
-    @Lazy
-    public SecurityConfig(JwtAuthFilter jwtAuthFilter, UserDao userDao, UserService userService) {
-        this.jwtAuthFilter = jwtAuthFilter;
-        this.userDao = userDao;
-        this.userService = userService;
-    }
+//    @Autowired
+//    @Lazy
+//    public SecurityConfig(JwtAuthFilter jwtAuthFilter, UserDao userDao, UserService userService) {
+//        this.jwtAuthFilter = jwtAuthFilter;
+//        this.userDao = userDao;
+//        this.userService = userService;
+//    }
 
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests()
                 .requestMatchers("/api/auth/**")
                 .permitAll()
-                .anyRequest()
-                .authenticated()
                 .and()
+                .authorizeHttpRequests(aut ->
+                        aut.requestMatchers("/patient/**").hasAuthority("PATIENT")
+                )
+                .authorizeHttpRequests(aut ->
+                        aut.requestMatchers("/doctor/**").hasAuthority("DOCTOR")
+                )
+                .authorizeHttpRequests(aut ->
+                        aut.requestMatchers("/secretaire/**").hasAuthority("SECRETERE")
+                )
+                .authorizeHttpRequests(aut ->
+                        aut.requestMatchers("/admin/**").hasAuthority("ADMIN")
+                )
+                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
@@ -89,5 +109,22 @@ public class SecurityConfig  {
 
             }
         };
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+
     }
 }
